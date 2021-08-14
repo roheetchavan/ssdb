41865f1fdb52cfdef8479ca633f31d86147f27a2
Rohit Chavan <roheetchavan@gmail.com>
2021-08-14 15:08:47 +0000

    Detecting command and statement.

--- main.c
+++ main.c
@@ -15,16 +15,31 @@ int main(int argc, char** argv)
 		print_db_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0)
+		//check if it's db command
+        if(input_buffer->buffer[0] == '.')
         {
-            clean_input_buffer(input_buffer);
-            printf("Closing db...\n");
-            exit(EXIT_SUCCESS);
+            switch(do_meta_command(input_buffer))
+			{
+				case META_COMMAND_SUCCESS:
+					continue;
+				case META_COMMAND_UNRECOGNZED:
+					printf("Unrecognized command '%s'\n", input_buffer->buffer);
+					continue;
+			}
         }
-        else
-        {
-            printf("Unrecognized command '%s'\n", input_buffer->buffer);
-        }
+
+		Statement statement;
+		switch(prepare_statement(input_buffer, &statement))
+		{
+			case PREPARE_SUCCESS:
+				break;
+			case PREPARE_UNRECOGNIZED_STATEMENT:
+				printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+				continue;
+		}
+
+		execute_statement(&statement);
+		printf("Executed\n");
     }
     return 0;
 }
